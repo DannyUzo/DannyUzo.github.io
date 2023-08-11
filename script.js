c602616de2f4dbe82db3e9c1a60df09ea72ae09d
Danny Uzo <uzodinmadaniel42@gmail.com>
2023-08-11 12:37:25 +0100

    new resturant page

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function show(){
+    menuWrapper.style.display = "block";
+}
+function hide(){
+
+    menuWrapper.style.display = "none";
+}
+
+function toggle(){
+
+    sidbar.style.right = "10%";
+}
+
+function toggleback(){
+
+    sidbar.style.right = "-15%";
+}
